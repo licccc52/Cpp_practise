607be9a0e490b6ed9ccd1bd4f3d55c0f9fd89b0f
li <1747257669@qq.com>
2024-03-27 12:00:04 +0800

    练习

--- threadPool2.cpp
+++ threadPool2.cpp
@@ -1,48 +1,52 @@
 #include<iostream>
 #include<thread>
+#include<thread>
 #include<mutex>
-#include<string>
-#include<condition_variable>
 #include<queue>
+#include<condition_variable>
+#include<unistd.h>
+#include<sys/syscall.h>
 
-std::queue<int> g_queue;
-std::condition_variable g_cv;
+std::queue<int> mission;
 std::mutex mtx;
+std::condition_variable c_v;
 
-void Producer()
+void provider()
 {
-    for(int i=0; i < 10; i++)
-    {
-        std::unique_lock<std::mutex> lock(mtx);
-        g_queue.push(i);
-        //通知消费者来取任务
-        g_cv.notify_one();
-        std::cout << "push task : " << i << std::endl;
-    }
-    std::this_thread::sleep_for(std::chrono::microseconds(100));
+    int i = syscall(SYS_gettid);
+    std::unique_lock<std::mutex> lock(mtx);
+    mission.push(i);
+    std::cout << "PROVIDER add task : " <<  i  << std::endl;
+    lock.unlock();
+    c_v.notify_one();
+    sleep(100);
 }
 
-int Consumer()
-{//持续不断地从生产者中取出任务执行
-    while(1)
+
+void consumer()
+{
+    int id = syscall(SYS_gettid);
+    while(true)
     {
         std::unique_lock<std::mutex> lock(mtx);
-        //如果队列为空， 需要等待
-        //队列为空表示需要等待
-        g_cv.wait(lock, [](){
-            return !g_queue.empty();
+        c_v.wait(lock, [](){
+            return !mission.empty();
         });
-        int value = g_queue.front();
-        std::cout << " g_queue.pop()  : "<<value << std::endl;
-        g_queue.pop();
+        int temp = mission.front();
+        std::cout << "               Thread " << id <<" execute : " << temp << std::endl;
+        mission.pop();  
     }
 }
 
 int main()
 {
-    std::thread_t1(Producer);
-    std::thread_t2(Consumer);
-    t1.join();
+    std::thread t2(consumer);
+    for(int i=0; i < 10; i++)
+    {
+        std::thread t1(provider);
+        t1.detach();
+    }
+
     t2.join();
 
     return 0;
